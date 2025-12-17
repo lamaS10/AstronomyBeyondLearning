adc1ba8f1913e67be942866ccf2b40deb63abdce
lama <leema442@gmail.com>
2025-12-17 11:05:47 +0300

    added settings for new email

--- AstronomyBeyondLearning/AstronomyBeyondLearning/settings.py
+++ AstronomyBeyondLearning/AstronomyBeyondLearning/settings.py
@@ -140,7 +140,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
